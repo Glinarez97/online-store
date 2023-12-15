fdaba1bb4419f9d4cb876abe3596d3a504d1474b
Glinarez97 <148172969+Glinarez97@users.noreply.github.com>
2023-12-15 06:08:57 -0800

    Add files via upload

---
+++ footer.jsx
@@ -0,0 +1,12 @@
+import "./footer.css";
+
+function Footer(){
+
+    return(
+    <div className="footer">
+        <label>Organika Store, a really expensive store</label>
+    </div>
+    );
+}
+
+export default Footer;
---
+++ navbar.jsx
@@ -0,0 +1,12 @@
+import "./navbar.css";
+
+function Navbar()
+{
+    return(
+        <div>
+            Menu goes here
+        </div>
+    );
+}
+
+export default Navbar;
